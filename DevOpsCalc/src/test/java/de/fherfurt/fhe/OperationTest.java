ad7e70cb512d15b1e3088ceccbf751d5dc86efe0
YvesHirschmann <159157498+YvesHirschmann@users.noreply.github.com>
2025-05-19 12:42:01 +0200

    update OperationTest.java

--- DevOpsCalc/src/test/java/de/fherfurt/fhe/OperationTest.java
+++ DevOpsCalc/src/test/java/de/fherfurt/fhe/OperationTest.java
@@ -1,37 +1,45 @@
 package de.fherfurt.fhe;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class OperationTest {
 
+    private static final double NUM1 = 2.0;
+    private static final double NUM2 = 3.0;
+    private static final double NUM3 = 5.0;
+    private static final double NUM4 = 10.0;
+    private static final double NUM5 = 2.5;
+    private static final double NUM6 = 0.0;
+
     @Test
     void testAddition() {
-        Operation op = new Operation(2.0, 3.0, OperationType.ADD);
-        assertEquals(5.0, op.execute());
+        Operation op = new Operation(NUM1, NUM2, OperationType.ADD);
+        assertEquals(NUM3, op.execute());
     }
 
     @Test
     void testSubtraction() {
-        Operation op = new Operation(5.0, 2.0, OperationType.SUBTRACT);
-        assertEquals(3.0, op.execute());
+        Operation op = new Operation(NUM3, NUM1, OperationType.SUBTRACT);
+        assertEquals(NUM2, op.execute());
     }
 
     @Test
     void testMultiplication() {
-        Operation op = new Operation(4.0, 2.5, OperationType.MULTIPLY);
-        assertEquals(10.0, op.execute());
+        Operation op = new Operation(NUM4, NUM5, OperationType.MULTIPLY);
+        assertEquals(NUM4, op.execute());
     }
 
     @Test
     void testDivision() {
-        Operation op = new Operation(10.0, 2.0, OperationType.DIVIDE);
-        assertEquals(5.0, op.execute());
+        Operation op = new Operation(NUM4, NUM1, OperationType.DIVIDE);
+        assertEquals(NUM3, op.execute());
     }
 
     @Test
     void testDivisionByZeroThrowsException() {
-        Operation op = new Operation(10.0, 0.0, OperationType.DIVIDE);
+        Operation op = new Operation(NUM4, NUM6, OperationType.DIVIDE);
         assertThrows(ArithmeticException.class, op::execute);
     }
 }
